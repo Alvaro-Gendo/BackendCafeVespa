e3839a9db849a2261091048bb281565db6af8818
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-20 17:17:35 -0300

    configuramos un puerto para nuestro backend

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
+import express from "express";
+
+//crear una instancia de expres
+const app = express();
+
+//creamos una variable
+app.set("port", process.env.PORT || 4000);
+//usar el puerto
+app.listen(app.get("port"), () =>{
+    console.log("Puerto " + app.get("port"))
+})
+
 console.log("hola mundo")
