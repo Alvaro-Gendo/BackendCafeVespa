4321c3b312e89b935a60fc6c3224e5df9032f340
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-25 18:30:05 -0300

    agregamos la logica para crear un producto y obtener el arreglo de los productos

--- src/models/producto.js
+++ src/models/producto.js
@@ -1,7 +1,7 @@
 import mongoose, {Schema} from "mongoose";
 
 const productoSchema = new Schema({
-    nombreProdcuto:{
+    nombreProducto:{
         type: String,
         required: true,
         unique: true,
