3120c4964a56631229a62713a689ba1a7bddf3f7
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-10-28 18:30:04 -0300

    se agrego validaciones con express validator

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -5,12 +5,46 @@ import {
   editarProducto,
   listarProductos,
 } from "../controllers/productos.controllers";
+import { check } from "express-validator";
 
 const rutas = Router();
 
-rutas.route("/productos").get(listarProductos).post(crearProducto);
+rutas.route("/productos")
+.get(listarProductos)
+.post([
+  check("nombreProducto")
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .isLength({min:2, max:50})
+  .withMessage("El nombre del producto debe tener entre 2 y 50 caracteres"),
+  check("precio")
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .isNumeric()
+  .withMessage("EL precio debe ser un numero")
+  .custom((valor)=>{
+    if(valor >= 1 && valor <= 1000){
+      return true
+    }else{
+      throw new Error("El precio estar entre 1 y 1000")
+    }
+  }),
+  check("imagen")
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .matches(/^https?:\/\/[\w\-]+(\.[\w\-]+)+[/#?]?.*$/)
+  .withMessage("Ingresar URL valida"),
+  check("categoria")
+  .notEmpty()
+  .withMessage("Campo obligatorio")
+  .isIn(["Bebida caliente", "Bebida fria", "Dulce", "Salado"])
+  .withMessage("Campo obligatorio")
+],crearProducto);
 
-rutas.route("/productos/:id").get().put(editarProducto).delete(borrarProducto);
+rutas.route("/productos/:id")
+.get()
+.put(editarProducto)
+.delete(borrarProducto);
 
 export default rutas;
 
