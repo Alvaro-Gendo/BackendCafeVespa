d01893914a2075385ec5de4d731411ea7bab7f0d
Alvaro Ruiz Mejia <zhuone@hotmail.com>
2022-11-03 17:10:11 -0300

    agregue archivo de configuracion de vercel

--- src/database.js
+++ src/database.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
 
 //localhost: 127.0.0.1
-const url = "mongodb://localhost:27017/cafe-vespa"
+// const url = "mongodb://localhost:27017/cafe-vespa" //BD_local
+const url = "mongodb+srv://Gendo:Gendo9567@cluster0.nzjxqu6.mongodb.net/cafe-vespa";
 
 mongoose.connect(url)
 
